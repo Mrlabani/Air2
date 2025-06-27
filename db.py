c4182ac81ba961c796c2ed41c76cc524ebdf78b4
Labani Behera 🎯 <158763270+Mrlabani@users.noreply.github.com>
2025-06-27 16:52:11 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.environ.get("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["torrentBot"]
+downloads = db["downloads"]
+
+def add_download(data):
+    downloads.insert_one(data)
+
+def get_all_downloads():
+    return list(downloads.find({}).sort("timestamp", -1))
+
+def clear_downloads():
+    downloads.delete_many({})
+  
